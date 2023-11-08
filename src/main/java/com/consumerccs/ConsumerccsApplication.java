5b62ab0e5cbc419cdeb634bf11fc95586aace897
racrivelari <ramon.crivelari@gmail.com>
2023-11-07 23:31:05 -0300

    feat: initial config

---
+++ src/main/java/com/consumerccs/ConsumerccsApplication.java
@@ -0,0 +1,13 @@
+package com.consumerccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerccsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerccsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consumerccs/ConsumerccsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumerccs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerccsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
